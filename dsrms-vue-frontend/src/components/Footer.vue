e9a6bfd3d6684daee9e6a520e9f096de697b92d8
BigtoC <taitocdt@gmail.com>
2020-04-28 19:14:29 +0800

    Added footer

---
+++ dsrms-vue-frontend/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <p>Design and written by Chan Tai To</p>
+    <p>Find source code on
+      <el-link icon="el-icon-view" type="primary" underline="false" style="font-size: large;"
+               href="https://github.com/BigtoC/Distributed-Students-Record-Management-System">
+        GitHub
+      </el-link>
+    </p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+  .footer {
+    background: #E0E0E0;
+    margin-top: 3%;
+    text-align: center;
+    padding: 5%;
+    font-size: large;
+  }
+</style>
